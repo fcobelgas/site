4d6da8ab836a59a70929c0ebfb354940ea3acaa7
FRANCISCO BELGAS <fcobelgashow@gmail.com>
2021-06-03 21:44:42 -0300

    Update in code for win initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# portifolio
+developer project portifolio.
+
+## Processo Dev
+
+Documentodo no GitHub
+
+## icon progress
+
+:new: New Function
+:bug: Fix bug
+:up: Update
+:heavy_check_mark: Finish
+:white_check_mark: Release
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belgas Projetos</title>
+    <link rel="stylesheet" href="css/css.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="ico/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="ico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="ico/favicon-16x16.png">
+    <link rel="manifest" href="ico/site.webmanifest">
+</head>
+<body>
+    <div class="container">
+    <header>
+        <div class="img-wrapper">
+        <img src="img/bg.jpg" alt="developer_bg">
+        </div>
+        <div class="banner">           
+            <h1>Desenvolvimento de Aplicações WEB</h1>
+            <p>Aqui voce encontra o que precisa para seu projeto web, pergunte-me como que entregamos o projeto pronto.</p>
+            <button>Contato</button>
+        </div>
+    </header>
+
+
+    </div>
+</body>
+</html>
